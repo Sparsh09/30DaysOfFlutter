5f23567f1d9d06016c0e9bace7863c1ae57d8cad
Sparsh09 <sparsh1698.cse18@chitkara.edu.in>
2021-12-24 12:59:21 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // bringMobile(isIos :true); //* Methods in dart *//
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.red),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/" : (context) => HomePage(),
+        "/login" : (context) => LoginPage(),
+        
+      },
     );
   }
+  //  bringMobile({ required bool isIos ,int price = 10000}) {}
 }
